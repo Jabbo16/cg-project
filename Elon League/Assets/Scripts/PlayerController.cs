54a000bd52b45215183d59fd55dd67811b438035
Juan Machuca <juan0machuca@hotmail.com>
2019-04-18 19:10:24 +0200

    Camera final version

--- Elon League/Assets/Scripts/PlayerController.cs
+++ Elon League/Assets/Scripts/PlayerController.cs
@@ -11,19 +11,29 @@ public class PlayerController : MonoBehaviour
 
     public CharacterController controller;
     private Vector3 moveDirection;
-    
+
+    public Transform relativeTransform;
+    private Vector3 vector_blue;
+    private Vector3 vector_red;
+
+    public bool fixedCamera = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        moveSpeed = 10;
-        jumpForce = 6;
+        moveSpeed = 20;
+        jumpForce = 200;
         //theRB = GetComponent<Rigidbody>();
         controller = GetComponent<CharacterController>();
+
+        vector_blue = relativeTransform.forward;
+        vector_red = relativeTransform.right;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         /*theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
 
         if (Input.GetButtonDown("Jump"))
@@ -31,19 +41,45 @@ public class PlayerController : MonoBehaviour
             theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
         }*/
 
-        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+        if (Input.GetMouseButtonDown(1)) fixedCamera = true;
 
-        if (controller.isGrounded)
-        {
+        if (Input.GetMouseButtonUp(1)){
+            fixedCamera = false;
+            vector_blue = relativeTransform.forward;
+            vector_red = relativeTransform.right;
+        }
+
+        if (fixedCamera){
+          if(Input.GetKey(KeyCode.W)) moveDirection = relativeTransform.forward * moveSpeed;
+          if(Input.GetKey(KeyCode.S)) moveDirection = -relativeTransform.forward * moveSpeed;
+          if(Input.GetKey(KeyCode.A)) moveDirection = -relativeTransform.right * moveSpeed;
+          if(Input.GetKey(KeyCode.D)) moveDirection = relativeTransform.right * moveSpeed;
+        }
+        else {
+          //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+          if(Input.GetKey(KeyCode.W)) moveDirection = vector_blue * moveSpeed;
+          if(Input.GetKey(KeyCode.S)) moveDirection = -vector_blue * moveSpeed;
+          if(Input.GetKey(KeyCode.A)) moveDirection = -vector_red * moveSpeed;
+          if(Input.GetKey(KeyCode.D)) moveDirection = vector_red * moveSpeed;
+        }
+
+
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) moveDirection = new Vector3(0, 0, 0);
+
+        //moveDirection.y = 0f;
+
+        if (controller.isGrounded) {
+
             moveDirection.y = 0f;
-            if (Input.GetButtonDown("Jump"))
-            {
+
+            if (Input.GetButtonDown("Jump")){
+
                 moveDirection.y = jumpForce;
             }
         }
+
         moveDirection.y = moveDirection.y + (Physics.gravity.y * Time.deltaTime);
 
         controller.Move(moveDirection * Time.deltaTime);
     }
-    
 }
---
+++ Elon League/Assets/Scripts/PushBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushBall : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+   {
+       //rb.MovePosition(transform.position + transform.forward * Time.deltaTime);
+   }
+}
