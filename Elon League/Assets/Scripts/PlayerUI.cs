ce70a64eeee98b21fa0e972d7e16b72c01456707
Mario Sánchez García <mario.sangar@hotmail.com>
2019-05-04 21:08:52 +0200

    show name label using PUN

--- Elon League/Assets/Scripts/PlayerUI.cs
+++ Elon League/Assets/Scripts/PlayerUI.cs
@@ -6,6 +6,14 @@ using UnityEngine.UI;
 public class PlayerUI : MonoBehaviour
 {
 
+    #region Public Fields
+    
+        [Tooltip("Pixel offset from the player target")]
+        [SerializeField]
+        public Vector3 screenOffset = new Vector3(0f,50f,0f);
+
+    #endregion
+
     #region Private Fields
 
         [Tooltip("UI Text to display Player's Name")]
@@ -15,9 +23,10 @@ public class PlayerUI : MonoBehaviour
         [Tooltip("Player target")]
         private PlayerManager target;
 
-        [Tooltip("Pixel offset from the player target")]
-        [SerializeField]
-        private Vector3 screenOffset = new Vector3(0f,30f,0f);
+        float characterControllerHeight = 0f;
+        Transform targetTransform;
+        Renderer targetRenderer;
+        Vector3 targetPosition;
 
     #endregion
 
@@ -38,11 +47,29 @@ public class PlayerUI : MonoBehaviour
             }
         }
 
+        void LateUpdate() {
+            // Do not show the UI if we are not visible to the camera, thus avoid potential bugs with seeing the UI, but not the player itself.
+            if (targetRenderer != null) {
+                // Debug.Log("Taget renderer: " + targetRenderer.isVisible.ToString());
+                // this.gameObject.SetActive(targetRenderer.isVisible);
+                this.gameObject.SetActive(true);
+            }
+
+            // #Critical
+            // Follow the Target GameObject on screen.
+            if (targetTransform != null) {
+                targetPosition = targetTransform.position;
+                targetPosition.y += characterControllerHeight;
+                this.transform.position = Camera.main.WorldToScreenPoint (targetPosition) + screenOffset;
+            }
+        }
+
     #endregion
 
     #region Public Methods
 
     public void SetTarget(PlayerManager _target) {
+        Debug.Log("SetTarget called");
 
         if (_target == null) {
             Debug.LogError("<Color=Red>Missing</Color> PlayMakerManager target for PlayerUI.SetTarget.");
@@ -52,6 +79,14 @@ public class PlayerUI : MonoBehaviour
         target = _target;
         if (playerNameText != null) {
             playerNameText.text = target.photonView.Owner.NickName;
+        }
+
+        targetTransform = this.target.GetComponent<Transform>();
+        targetRenderer = this.target.GetComponent<Renderer>();
+        CharacterController characterController = _target.GetComponent<CharacterController> ();
+        // Get data from the Player that won't change during the lifetime of this Component
+        if (characterController != null) {
+            characterControllerHeight = characterController.height;
         }
     }
 
